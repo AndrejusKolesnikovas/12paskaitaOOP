5aa86593c73e8bac4a36c39ac3e495818909da6a
AndrejusKolesnikovas <andrejuskolesnikovas@yahoo.com>
2022-09-06 20:42:50 +0300

    passed by reference, passed by value

---
+++ lesson2/2_passing_by_reference.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+class Product
+{
+    public int $price;
+}
+
+class ProductModifier
+{
+    // objektai yra passed by reference. Tai reiskia, kad i metoda yra paduodamas "originalas" ty. pats objektas
+    public function modify(Product $product): void
+    {
+        $product->price = 100;
+    }
+
+    // int, float, string ir array yra passed by value. Tai reiskia, kad i metoda yra paduodama kopija, ne "originalas"
+    public function modifyProductArray(array $product): void
+    {
+        $product['price'] = 100;
+    }
+}
+
+$product = new Product();
+$product->price = 99;
+echo $product->price; // 99
+$productArray = ['price' => 99];
+print_r($product);
+$productModifier = new ProductModifier();
+$productModifier->modify($product);
+var_dump($productModifier->modifyProductArray($productArray));
+print_r($productArray);
+
+
